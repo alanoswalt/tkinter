b0fd9868c534b84af7dbeb1be537f48582d7237b
Alan <alanoswalt01@gmail.com>
2023-07-10 20:01:14 -0600

    extra stuff

--- tkinter_basics_2.py
+++ tkinter_basics_2.py
@@ -7,19 +7,27 @@ root = Tk()
 
 #Function that gets called with button
 def myClick():
-    mylabel3 = Label(root, text='Hello World')
+    mylabel3 = Label(root, text=e.get())
     mylabel3.grid(row=1, column=1)
 
 #Creating a lablel widger
 mylabel1 = Label(root, text='Hello World')
 mylabel2 = Label(root, text='Soy Alan... de nuevo')
+
+#Creating buttons
 myButton=Button(root, text="Buen clickeo", padx=50,pady=60, command = myClick)
 
+#Creating entries or texboxes
+e=Entry(root, bg='blue')
+
+e.get() #This gets the value in the textbox
+e.insert(0, 'Random text') #This allows to enter random text in the textbox
 
 #Shoving in onto the screen
 mylabel1.grid(row=0, column=0)
 mylabel2.grid(row=1, column=0)
-myButton.grid(row=2,column=0)
+
+e.grid(row=0, column=2)
 
 
 
