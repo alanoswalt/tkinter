669edc8cf3e463175a36e65f6a9ab8a89df9daae
Alan <alanoswalt01@gmail.com>
2023-07-13 20:33:19 -0600

    Calculator with and without class

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,76 +1,70 @@
+#Calculator Version 1.0
 from tkinter import *
 
 
 root = Tk()
 root.title('Calculator')
 
+class operations_Calculator:
 
-def calculate(sign):
-    if sign == '+':
-        result = num1+num2
 
-    if sign == '-':
-        result = num1+num2
-    if sign == '+':
-        result = num1+num2
-    if sign == '+':
-        result = num1+num2
-    if sign == '+':
-        result = num1+num2
-
-operation = ''
-
-def button_click(button):
-    #textbox.delete(0, END)
-    if isinstance(button, int):
+    def button_click(self, button):
         current=textbox.get()
         textbox.delete(0, END)
         textbox.insert(0, str(current)+str(button))
-    #elif button=='c':
-    #    textbox.delete(0, END)
+        #elif button=='c':
+        #    textbox.delete(0, END)
 
-def button_clear():
-    #textbox.delete(0, END)
-    textbox.delete(0, END)
+    def button_clear(self):
+        #textbox.delete(0, END)
+        textbox.delete(0, END)
 
-def button_add():
-    first_number=textbox.get()
-    global f_num
-    f_num = int(first_number)
-    textbox.delete(0, END)
+    def button_operation(self, op):
+        global f_num
+        f_num= float(textbox.get())
+        textbox.delete(0, END)
+        global operation
+        operation = op
+
+    def button_equal(self):
+        first_number=float(f_num)
+        second_number=float(textbox.get())
+        textbox.delete(0, END)
+        if operation == '+':
+            result = first_number+second_number
+        if operation == '-':
+            result = first_number-second_number
+        if operation == '*':
+            result = first_number*second_number
+        if operation == '/':
+            result = first_number/second_number
+        textbox.insert(0,result)
 
 
-def button_equal():
-    first_number=f_num
-    second_number=int(textbox.get())
-    textbox.delete(0, END)
-    result = first_number+second_number
-    textbox.insert(0,result)
-
-
+calculator=operations_Calculator()
 
 #textbox
 textbox=Entry(width=35, borderwidth=5)
 
 #buttons
-button9=Button(root, text="9", padx=20, pady=20, command=lambda: button_click(9))
-button8=Button(root, text="8", padx=20, pady=20, command=lambda: button_click(8))
-button7=Button(root, text="7", padx=20, pady=20, command=lambda: button_click(7))
-button6=Button(root, text="6", padx=20, pady=20, command=lambda: button_click(6))
-button5=Button(root, text="5", padx=20, pady=20, command=lambda: button_click(5))
-button4=Button(root, text="4", padx=20, pady=20, command=lambda: button_click(4))
-button3=Button(root, text="3", padx=20, pady=20, command=lambda: button_click(3))
-button2=Button(root, text="2", padx=20, pady=20, command=lambda: button_click(2))
-button1=Button(root, text="1", padx=20, pady=20, command=lambda: button_click(1))
-button0=Button(root, text="0", padx=20, pady=20, command=lambda: button_click(0))
-buttonEq=Button(root, text="=", padx=20, pady=20, command=lambda: button_equal())
-buttonPlus=Button(root, text="+", padx=20, pady=20, command=lambda: button_add())
-ButtonMinus=Button(root, text="-", padx=20, pady=20, command=lambda: button_click('-'))
-buttonDiv=Button(root, text="/", padx=20, pady=20, command=lambda: button_click('/'))
-buttonMult=Button(root, text="*", padx=20, pady=20, command=lambda: button_click('*'))
-buttonDot=Button(root, text=".", padx=20, pady=20, command=lambda: button_click('.'))
+button9=Button(root, text="9", padx=20, pady=20, command=lambda: calculator.button_click(9))
+button8=Button(root, text="8", padx=20, pady=20, command=lambda: calculator.button_click(8))
+button7=Button(root, text="7", padx=20, pady=20, command=lambda: calculator.button_click(7))
+button6=Button(root, text="6", padx=20, pady=20, command=lambda: calculator.button_click(6))
+button5=Button(root, text="5", padx=20, pady=20, command=lambda: calculator.button_click(5))
+button4=Button(root, text="4", padx=20, pady=20, command=lambda: calculator.button_click(4))
+button3=Button(root, text="3", padx=20, pady=20, command=lambda: calculator.button_click(3))
+button2=Button(root, text="2", padx=20, pady=20, command=lambda: calculator.button_click(2))
+button1=Button(root, text="1", padx=20, pady=20, command=lambda: calculator.button_click(1))
+button0=Button(root, text="0", padx=20, pady=20, command=lambda: calculator.button_click(0))
+buttonEq=Button(root, text="=", padx=20, pady=20, command=lambda: calculator.button_equal())
+buttonPlus=Button(root, text="+", padx=20, pady=20, command=lambda: calculator.button_operation('+'))
+ButtonMinus=Button(root, text="-", padx=20, pady=20, command=lambda: calculator.button_operation('-'))
+buttonDiv=Button(root, text="/", padx=20, pady=20, command=lambda: calculator.button_operation('/'))
+buttonMult=Button(root, text="*", padx=20, pady=20, command=lambda: calculator.button_operation('*'))
+buttonDot=Button(root, text=".", padx=20, pady=20, command=lambda: calculator.button_click('.'))
 
-buttonClear=Button(root, text="Clear", padx=80 ,pady=20, command=lambda: button_clear())
+buttonClear=Button(root, text="Clear", padx=80 ,pady=20, command=lambda: calculator.button_clear())
 
 
 #Buttons in screen
@@ -92,7 +86,6 @@ ButtonMinus.grid(row=2,column=3)
 buttonMult.grid(row=3,column=3)
 buttonDiv.grid(row=4,column=3)
 buttonClear.grid(row=5,column=0,columnspan=4)
-
 
 
 root.mainloop()
---
+++ calculator/calculator2.0.py
@@ -0,0 +1,86 @@
+#Calculator Version 1.0
+from tkinter import *
+
+
+
+class operations_Calculator:
+
+    def __init__(self):
+        root = Tk()
+        root.title('Calculator')
+        #textbox
+        self.f_num=0
+        self.operation=''
+        self.textbox=Entry(width=35, borderwidth=5)
+        button9=Button(root, text="9", padx=20, pady=20, command=lambda: self.button_click(9))
+        button8=Button(root, text="8", padx=20, pady=20, command=lambda: self.button_click(8))
+        button7=Button(root, text="7", padx=20, pady=20, command=lambda: self.button_click(7))
+        button6=Button(root, text="6", padx=20, pady=20, command=lambda: self.button_click(6))
+        button5=Button(root, text="5", padx=20, pady=20, command=lambda: self.button_click(5))
+        button4=Button(root, text="4", padx=20, pady=20, command=lambda: self.button_click(4))
+        button3=Button(root, text="3", padx=20, pady=20, command=lambda: self.button_click(3))
+        button2=Button(root, text="2", padx=20, pady=20, command=lambda: self.button_click(2))
+        button1=Button(root, text="1", padx=20, pady=20, command=lambda: self.button_click(1))
+        button0=Button(root, text="0", padx=20, pady=20, command=lambda: self.button_click(0))
+        buttonEq=Button(root, text="=", padx=20, pady=20, command=lambda: self.button_equal())
+        buttonPlus=Button(root, text="+", padx=20, pady=20, command=lambda: self.button_operation('+'))
+        ButtonMinus=Button(root, text="-", padx=20, pady=20, command=lambda: self.button_operation('-'))
+        buttonDiv=Button(root, text="/", padx=20, pady=20, command=lambda: self.button_operation('/'))
+        buttonMult=Button(root, text="*", padx=20, pady=20, command=lambda: self.button_operation('*'))
+        buttonDot=Button(root, text=".", padx=20, pady=20, command=lambda: self.button_click('.'))
+        buttonClear=Button(root, text="Clear", padx=80 ,pady=20, command=lambda: self.button_clear())
+
+        #Buttons in screen
+        self.textbox.grid(row=0,column=0,columnspan=4)#esto ultimo toma el tamaño de las columnas
+        button9.grid(row=1,column=2)
+        button8.grid(row=1,column=1)
+        button7.grid(row=1,column=0)
+        button6.grid(row=2,column=2)
+        button5.grid(row=2,column=1)
+        button4.grid(row=2,column=0)
+        button3.grid(row=3,column=2)
+        button2.grid(row=3,column=1)
+        button1.grid(row=3,column=0)
+        button0.grid(row=4,column=0)
+        buttonDot.grid(row=4,column=1)
+        buttonEq.grid(row=4,column=2)
+        buttonPlus.grid(row=1,column=3)
+        ButtonMinus.grid(row=2,column=3)
+        buttonMult.grid(row=3,column=3)
+        buttonDiv.grid(row=4,column=3)
+        buttonClear.grid(row=5,column=0,columnspan=4)
+
+        root.mainloop()
+
+    def button_click(self, button):
+        current=self.textbox.get()
+        self.textbox.delete(0, END)
+        self.textbox.insert(0, str(current)+str(button))
+        #elif button=='c':
+        #    textbox.delete(0, END)
+
+    def button_clear(self):
+        #textbox.delete(0, END)
+        self.textbox.delete(0, END)
+
+    def button_operation(self, op):
+        self.f_num= float(self.textbox.get())
+        self.textbox.delete(0, END)
+        self.operation = op
+
+    def button_equal(self):
+        first_number=float(self.f_num)
+        second_number=float(self.textbox.get())
+        self.textbox.delete(0, END)
+        if self.operation == '+':
+            result = first_number+second_number
+        if self.operation == '-':
+            result = first_number-second_number
+        if self.operation == '*':
+            result = first_number*second_number
+        if self.operation == '/':
+            result = first_number/second_number
+        self.textbox.insert(0,result)
+
+
+calculator=operations_Calculator()
